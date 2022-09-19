7a147dcbc9fad90a8be04c3dcc912730ee8307b7
DOUGLAS <douglasbelloalv1@outlook.com>
2022-09-19 11:29:56 -0300

    aula07 adicionada

--- aula07-functions.php
+++ aula07-functions.php
@@ -20,4 +20,40 @@ function boas_vindas($nome) {
 }
 $name = "joão";
 boas_vindas($name); 		// pode colocar o valor direto na função, exemplo: boas_vindas("douglas");
+echo "<hr>";
+
+function media($trimestre1,$trimestre2,$trimestre3) {
+	$calculo = ($trimestre1 + $trimestre2 + $trimestre3) / 3;
+	$calculo = round($calculo, 2);			// arredonda para 2 casas decimais depois da vírgula
+	return $calculo;
+}
+
+$nota1 = 10;
+$nota2 = 5;
+$nota3 = 7;
+$notaMedia = media($nota1,$nota2,$nota3);
+echo $notaMedia;
+echo "<hr>";
+function aprovado_ou_reprovado($notaAluno) {
+	if ($notaAluno >= 7) {
+		$retorno = "aprovado";
+	} else {
+		$retorno = "reprovado";
+	}
+	return $retorno;
+}
+
+echo aprovado_ou_reprovado($notaMedia);
+echo "<hr>";
+function lista($lista) {
+	$resultado = "<ul>";
+	foreach ($lista as $value) {
+		$resultado .= "<li>" . $value . "</li>";
+
+	}
+	$resultado .= "</ul>";
+	return $resultado;
+}
+$lista = ["Pão","Café","Queijo"];
+echo lista($lista);
 ?>
