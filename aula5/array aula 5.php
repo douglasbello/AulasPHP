41d88d5b94a7a364d5bc1740a00b0138f450f1a5
DOUGLAS <douglasbelloalv1@outlook.com>
2022-09-19 09:02:07 -0300

    teste

---
+++ aula5/array aula 5.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$dragonBall = ["Goku", "Vegeta", "Trunks", "Piccolo", "Gohan"];
+
+in_array("Vegeta", $dragonBall);
+$busca = "Trunks";
+if (in_array($busca, $dragonBall)) {
+	echo "Encontrei o valor:" .$busca;
+} else {
+	echo "Valor não encontrado";
+}
+echo "<hr>";
+
+$juntar = implode("-", $dragonBall);
+echo $juntar;
+
+$frase = "Hoje no almoço eu comi:"
+
+?>
+
+
